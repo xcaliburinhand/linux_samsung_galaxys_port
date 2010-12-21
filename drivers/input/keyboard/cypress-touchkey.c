59b8a36d8a6ceb04a7c00cfa0ed829d29d754f8f
Zach Dunton <fenfir@gmail.com>
2010-12-20 23:22:47 -0600

    Fixed capacitive keys

--- drivers/input/keyboard/cypress-touchkey.c
+++ drivers/input/keyboard/cypress-touchkey.c
@@ -306,7 +306,8 @@ static int cypress_touchkey_probe(struct i2c_client *client,
 	}
 
 	devdata->has_legacy_keycode = data[1] >= 0xc4 || data[1] < 0x9 ||
-					(data[1] == 0x9 && data[2] < 0x9);
+					(data[1] == 0x9 && data[2] < 0x9)
+					data[1] == 0xe;
 
 	err = i2c_touchkey_write_byte(devdata, devdata->backlight_on);
 	if (err) {
