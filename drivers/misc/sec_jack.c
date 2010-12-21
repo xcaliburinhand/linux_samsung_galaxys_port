b2ca7bca0dcae9558464c2f7443ee81cece72da2
xcaliburinhand <github@muteheadlight.net>
2010-12-21 02:36:12 -0500

    captivate headset logic fix

--- drivers/misc/sec_jack.c
+++ drivers/misc/sec_jack.c
@@ -275,6 +275,10 @@ static int sec_jack_probe(struct platform_device *pdev)
 
 	dev_set_drvdata(&pdev->dev, hi);
 
+#if defined(CONFIG_GALAXYS_SGH_I897)
+        pdata->det_active_high = 1;
+#endif
+
 	return 0;
 
 err_enable_irq_wake:
